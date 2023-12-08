38e6158ae8ff1df86a7d773f4c4a2c4ebeeeb55b
Talia Baker <taliabaker@Talias-MacBook-Air.local>
2023-12-07 22:50:58 -0500

    Updated SQL

--- schema.sql
+++ schema.sql
@@ -22,32 +22,34 @@ CREATE TABLE `Users` (
 -- Contacts Table
 --
 
-CREATE TABLE Contacts(
-'id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-'title' VARCHAR(255) NOT NULL default '',
-'firstname'  VARCHAR(255) NOT NULL default '',
-'lastname' VARCHAR(255) NOT NULL default '',
-'email' VARCHAR(510) NOT NULL default '',
-'telephone' VARCHAR(255) NOT NULL default '',
-'company'  VARCHAR(255) NOT NULL default '',
-'type' VARCHAR(255) NOT NULL default '',
-'assigned_to'  INT NOT NULL default '',
-'created_by'  INT NOT NULL default '',
-'created_at' DATETIME DEFAULT CURRENT_TIMESTAMP,
-'updated_at' TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE Contacts (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(255) NOT NULL DEFAULT '',
+    `firstname` VARCHAR(255) NOT NULL DEFAULT '',
+    `lastname` VARCHAR(255) NOT NULL DEFAULT '',
+    `email` VARCHAR(510) NOT NULL DEFAULT '',
+    `telephone` VARCHAR(255) NOT NULL DEFAULT '',
+    `company` VARCHAR(255) NOT NULL DEFAULT '',
+    `type` VARCHAR(255) NOT NULL DEFAULT '',
+    `assigned_to` INT NOT NULL DEFAULT 0, -- Modify default value as needed
+    `created_by` INT NOT NULL DEFAULT 0, -- Modify default value as needed
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
 
 --
 -- Notes Table
 --
 
-CREATE TABLE Notes(
-'id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-'contact_id'  NOT NULL default '',
-'comment' TEXT,
-'created_by'  INT(25)NOT NULL default '',
-'created_at' DATETIME DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE Notes (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `contact_id`INT NOT NULL, -- Adjust as needed
+  `comment` TEXT,
+  `created_by` INT NOT NULL, -- Adjust as needed
+  `created_at`DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
 
 INSERT INTO Users (firstname,lastname,password,email,role,created_at) VALUES
 ('Jan','Levinson','passwordJAN','jan.levinson@paper.co','Member',NOW()),
